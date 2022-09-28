d0f077bab969f676c53be4586d93fbad096eef3d
ling2e <limxinze@gmail.com>
2022-09-28 22:52:04 +0800

    server changed

--- public/scripts/_setting.js
+++ public/scripts/_setting.js
@@ -1,5 +1,5 @@
 // export const serverUrl = "http://localhost:5000/api/"
 // export const server = "http://localhost:5000/"
 
-export const serverUrl = "https://cp-therapy.herokuapp.com/api/"
-export const server = "https://cp-therapy.herokuapp.com/"
+export const server = "https://cp-therepy-backend.onrender.com"
+export const serverUrl = server + "/api/"
